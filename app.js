f693eca630baf70616846bfdcfa20773f4966607
FaridAliyev-dev <farid.aliyevv258@gmail.com>
2024-12-17 09:23:44 +0400

    task questions

--- app.js
+++ app.js
@@ -15,21 +15,24 @@ let arr = [
 ];
 
 // 1) "name"-i "t" herfi ile bashlayan obyektleri yeni arraya yigin (filter)
-let startsWithT = arr.filter(obj => obj.name.startsWith("t"));
+let startsWithT = arr.filter((obj) => obj.name.startsWith("t"));
 console.log("1) Starts with 't':", startsWithT);
 
 // 2) "name"-i "t" herfi ile bashlayib "t" herfi ile biten obyektlerin sayini tapin (forEach)
 let countStartsAndEndsWithT = 0;
-arr.forEach(obj => {
+arr.forEach((obj) => {
   if (obj.name.startsWith("t") && obj.name.endsWith("t")) {
     countStartsAndEndsWithT++;
   }
 });
-console.log("2) Count of names starting and ending with 't':", countStartsAndEndsWithT);
+console.log(
+  "2) Count of names starting and ending with 't':",
+  countStartsAndEndsWithT
+);
 
 // 3) "name"-i "t" herfi ile bashlayib "t" herfi ile biten obyektlerin "key"lerinin cemini tapin
 let sumKeys = 0;
-arr.forEach(obj => {
+arr.forEach((obj) => {
   if (obj.name.startsWith("t") && obj.name.endsWith("t")) {
     sumKeys += obj.key;
   }
@@ -37,7 +40,7 @@ arr.forEach(obj => {
 console.log("3) Sum of keys (starts and ends with 't'):", sumKeys);
 
 // 4) "name"-i "e" herfi ile biten obyeklerdeki name-in deyerini "SuperDev" sozu ile evezleyin.
-let replacedNames = arr.map(obj => {
+let replacedNames = arr.map((obj) => {
   if (obj.name.endsWith("e")) {
     return { ...obj, name: "SuperDev" };
   }
@@ -46,7 +49,10 @@ let replacedNames = arr.map(obj => {
 console.log("4) Replace names ending with 'e':", replacedNames);
 
 // 5) "name"-i en uzun olan obyekti tapin
-let longestNameObj = arr.reduce((longest, obj) => obj.name.length > longest.name.length ? obj : longest, arr[0]);
+let longestNameObj = arr.reduce(
+  (longest, obj) => (obj.name.length > longest.name.length ? obj : longest),
+  arr[0]
+);
 console.log("5) Object with the longest name:", longestNameObj);
 
 // 6) "name"-i en uzun olan obyektin key'ni tapin
@@ -54,22 +60,36 @@ console.log("6) Key of the object with the longest name:", longestNameObj.key);
 
 // 7) "name"-i en uzun olan obyektin indexin kvadratini hesablayin
 
-
 // 8) "name"-inin uzunlugu 4 olan obyektlerden ibaret yeni array yaradin. (filter)
-let length4Names = arr.filter(obj => obj.name.length === 4);
+let length4Names = arr.filter((obj) => obj.name.length === 4);
 console.log("8) Names with length 4:", length4Names);
 
 // 9) En boyuk "key"i olan obyektin "name"-ini tapin
-let maxKeyObj = arr.reduce((max, obj) => obj.key > max.key ? obj : max, arr[0]);
+let maxKeyObj = arr.reduce(
+  (max, obj) => (obj.key > max.key ? obj : max),
+  arr[0]
+);
 console.log("9) Name of the object with the largest key:", maxKeyObj.name);
 
 // 10) Terkibinde 2 'l' (el) herfi olan obyekt(ler)i tapin.
-let containsTwoLs = arr.filter(obj => (obj.name.match(/l/g) || []).length === 2);
-console.log("10) Objects with exactly two 'l's:", containsTwoLs);
-
+arr.forEach((item) => {
+  let lCount = 0;
+  [...item.name].forEach((letter) => {
+    if (letter.toLocaleLowerCase() === "l") {
+      lCount++;
+    }
+  });
+  if (lCount === 2) {
+    console.log(item);
+  }
+});
 // 11) Terkibinde ən az 2 't' herfi olan obyekt(ler)i tapın.
 
-
 // 12) Key'leri 10'dan boyuk ve "name"-i 'l' herfi ile bashlayan obyektleri tapaq
-let keyGreaterThan10AndStartsWithL = arr.filter(obj => obj.key > 10 && obj.name.startsWith("l"));
-console.log("12) Objects with key > 10 and name starting with 'l':", keyGreaterThan10AndStartsWithL);
+let keyGreaterThan10AndStartsWithL = arr.filter(
+  (obj) => obj.key > 10 && obj.name.startsWith("l")
+);
+console.log(
+  "12) Objects with key > 10 and name starting with 'l':",
+  keyGreaterThan10AndStartsWithL
+);
