b76faf7b78453abd107a15db763d016f15f403a4
FaridAliyev-dev <farid.aliyevv258@gmail.com>
2024-12-20 09:48:08 +0400

    js fixes

--- ProductImageSelector/app.js
+++ ProductImageSelector/app.js
@@ -1,5 +1,7 @@
 const mainImageContainer = document.querySelector(".containerItems img");
-const selectors = document.querySelectorAll(".imgSelector .selector, .imgSelector .selector1, .imgSelector .selector2");
+const selectors = document.querySelectorAll(
+  ".imgSelector .selector, .imgSelector .selector1, .imgSelector .selector2"
+);
 selectors.forEach((selector) => {
   selector.addEventListener("click", () => {
     const clickedImageSrc = selector.querySelector("img").getAttribute("src");
